eecac4c2c7dbb4d4ced1015840847586df6b8de7
vasifalasgarzade <vasiffa@code.edu.az>
2021-01-27 02:27:39 -0800

    add

---
+++ VaVuBaku/Data/VaVuDb.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VaVuBaku.Models;
+
+
+namespace VaVuBaku.Data
+{
+    public class VaVuDb:DbContext
+    {
+        public VaVuDb(DbContextOptions<VaVuDb> options):base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
